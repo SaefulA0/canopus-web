c0878846093eb21fb30b9f168c4faa5c50509ceb
ImsanInBorderland <67819442+IkhsanNurulRizki@users.noreply.github.com>
2023-01-21 20:30:42 +0700

    perbaikan

--- components/cards/cardKontenKerdil.js
+++ components/cards/cardKontenKerdil.js
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function CardkontenKerdil(){
     return(
         <>
-        <div className="relative z-10 shadow-md cursor-pointer h-56 border-4 border-white rounded-xl">
+        <div className="relative z-10 shadow-md cursor-pointer h-2/3 border-4 border-white rounded-xl">
                 <p className="absolute top-4 right-14 font-normal text-sm text-opacity-40 text-white">Jarak:</p>
                 <p className="absolute top-9 right-5 font-normal text-sm text-opacity-40 text-white">Kamu disini</p>
                 
@@ -48,7 +48,7 @@ export default function CardkontenKerdil(){
                         <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"/>
                 </svg> */}
         </div>
-        <div className="relative z-10 shadow-md cursor-pointer border-4 h-56 border-white rounded-xl">
+        <div className="relative z-10 shadow-md cursor-pointer border-4 h-2/3 border-white rounded-xl">
                 <p className="absolute top-4 right-14 font-normal text-sm text-opacity-40 text-white">Jarak:</p>
                 <p className="absolute top-9 right-5 font-normal text-sm text-opacity-40 text-white">599 Juta KM</p>
                 <Image
