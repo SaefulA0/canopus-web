7c577969b6fc19b508e06f05b3c50e803f1e41dd
Saeful_A <saefulanwaroktariansah@gmail.com>
2023-02-12 16:08:19 +0700

    fix editContent

--- pages/dashboard/contents/editContent/[id].js
+++ pages/dashboard/contents/editContent/[id].js
@@ -361,7 +361,7 @@ export default function editContents({
                           })
                         }
                         type="text"
-                        name="trivia"
+                        name="videoId"
                         className="mt-1 px-3 py-2 border shadow-sm border-slate-300 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
                       />
                     </label>
